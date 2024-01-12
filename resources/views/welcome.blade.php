a98273014dbc00171dff5da927a50d67eb45bf62
[Bank] Rittichok <rittichok.fa@gmail.com>
2024-01-13 01:08:39 +0700

    up

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    สวัสดีครับ
                 </div>
 
                 <div class="links">
